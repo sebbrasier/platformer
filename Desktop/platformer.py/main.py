9370710dd0f07ff6eb0538b1a92ff6cf6e7d2a32
sébastien <sebastien.brasier@epfl.ch>
2025-02-27 10:46:33 +0100

    Création fenetre

--- Desktop/platformer.py/main.py
+++ Desktop/platformer.py/main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from platformer-py!")
+import arcade
 
+# Constants
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Platformer"
+
+class GameView(arcade.View):
+    """Main in-game view."""
+
+    def __init__(self) -> None:
+        # Magical incantion: initialize the Arcade view
+        super().__init__()
+
+        # Choose a nice comfy background color
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+        # Setup our game
+        self.setup()
+
+    def setup(self) -> None:
+        """Set up the game here."""
+        pass
+
+    def on_draw(self) -> None:
+        """Render the screen."""
+        self.clear() # always start with self.clear()
+
+def main() -> None:
+    """Main function."""
+
+    # Create the (unique) Window, setup our GameView, and launch
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    game_view = GameView()
+    window.show_view(game_view)
+    arcade.run()
 
 if __name__ == "__main__":
     main()
