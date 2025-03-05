5bfcf3cd74213d13a2f6097e365a1b0c7383f72d
sébastien <sebastien.brasier@epfl.ch>
2025-03-05 22:03:18 +0100

    déplacement sons 2

--- Desktop/platformer.py/gameview.py
+++ Desktop/platformer.py/gameview.py
@@ -150,11 +150,10 @@ class GameView(arcade.View):
         coin_hit = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
         for coin in coin_hit:
             coin.remove_from_sprite_lists()
-<<<<<<< HEAD
-=======
+
         #son
             arcade.play_sound(self.coin_sound)
->>>>>>> branche-seb
+
         
 
 
