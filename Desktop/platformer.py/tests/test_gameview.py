59fbc2728a29b3e500b0180bd17730ed9e097e04
Emerickpalot <emerick.palalot@epfl.ch>
2025-04-05 20:29:18 +0200

    test weapon

--- Desktop/platformer.py/tests/test_gameview.py
+++ Desktop/platformer.py/tests/test_gameview.py
@@ -2,6 +2,7 @@ import arcade
 
 from gameview import *
 from readmap import *
+import math
 
 
 #test ouverture du fichier map
@@ -233,6 +234,105 @@ def test_bat(window : arcade.Window) -> None:
     window.test(50)
     # Après collision, on attend que la vitesse soit inversée.
     assert bat1.speed == -vitesse_initiale
+
+def test_sword_orientation(window: arcade.Window) -> None:
+    # Initialiser la vue et la map de test
+    view = GameView()
+    view.setup("maps/map_tests/jump.txt")
+    window.show_view(view)
+    
+    # Forcer l'épée comme arme active (index 0)
+    view.active_weapon.index = 0
+    weapon = view.active_weapon.weapons[0]
+    
+
+    window.test(25)
+    
+    # Liste de cas à tester comme clic de souris
+    test_cases = [
+        (0, 0),
+        (100, 100),
+        (-100, -50),
+        (200, -150),
+        (-250, 300),
+        (1000, 1000),
+        (500, 0),
+        (700, -150),
+        (500, 300)
+    ]
+    # on calcul d'abord les coordonnées prévu avec nos calculs puis on verifie si c'est bien ou se trouve l'épée
+    for mouse_x, mouse_y in test_cases:
+        view.on_mouse_press(mouse_x, mouse_y, arcade.MOUSE_BUTTON_LEFT, 0)
+        
+        world_x = mouse_x + view.camera.position[0] - (WINDOW_WIDTH / 2)
+        world_y = mouse_y + view.camera.position[1] - (WINDOW_HEIGHT / 2)
+        ref_x = view.player_sprite.center_x
+        ref_y = view.player_sprite.center_y
+        angle = math.atan2(ref_x - world_x, ref_y - world_y)
+        expected_angle = math.degrees(angle) + 140
+        #on verifie si l'orientation est bonne avec une tolerance de 0,01 car on manipule des float
+        assert math.isclose(weapon.attribute.angle, expected_angle, abs_tol=0.001)
+        weapon.update_weapon_position()
+
+        expected_weapon_r = math.radians(expected_angle - 50)
+        expected_vec = (25 * math.cos(expected_weapon_r), 25 * math.sin(-expected_weapon_r))
+        expected_center_x = view.player_sprite.center_x + expected_vec[0]
+        expected_center_y = view.player_sprite.center_y - 15 + expected_vec[1]
+        #on verifie si la direction est bonne avec une tolerance de 0,01 car on manipule des float
+        assert math.isclose(weapon.attribute.center_x, expected_center_x, abs_tol=0.001)
+        assert math.isclose(weapon.attribute.center_y, expected_center_y, abs_tol=0.001)
+        window.test(10)
+
+def test_bow_orientation(window: arcade.Window) -> None:
+    # Initialiser la vue et la map de test
+    view = GameView()
+    view.setup("maps/map_tests/jump.txt")
+    window.show_view(view)
+    
+    # Forcer l'arc comme arme active (indice 1, supposant que 0 est l'épée)
+    view.active_weapon.index = 1
+    weapon = view.active_weapon.weapons[1]
+    
+    window.test(25)
+    
+    # Liste de cas à tester comme clic de souris
+    test_cases = [
+        (0, 0),
+        (100, 100),
+        (-100, -50),
+        (200, -150),
+        (-250, 300),
+        (1000, 1000),
+        (500, 0),
+        (700, -150),
+        (500, 300)
+    ]
+    # on calcul d'abord les coordonnées prévu avec nos calculs puis on verifie si c'est bien ou se trouve l'arc
+    for mouse_x, mouse_y in test_cases:
+        view.on_mouse_press(mouse_x, mouse_y, arcade.MOUSE_BUTTON_LEFT, 0)
+        
+        world_x = mouse_x + view.camera.position[0] - (WINDOW_WIDTH / 2)
+        world_y = mouse_y + view.camera.position[1] - (WINDOW_HEIGHT / 2)
+        ref_x = view.player_sprite.center_x
+        ref_y = view.player_sprite.center_y
+        angle = math.atan2(ref_x - world_x, ref_y - world_y)
+        expected_angle = math.degrees(angle) + 120
+        
+        #on verifie si l'orientation' est bonne avec une tolerance de 0,01 car on manipule des float
+        assert math.isclose(weapon.attribute.angle, expected_angle, abs_tol=0.001)
+        weapon.update_weapon_position()
+
+        expected_weapon_r = math.radians(expected_angle - 50)
+        expected_vec = (20 * math.cos(expected_weapon_r), 20 * math.sin(-expected_weapon_r))
+        expected_center_x = view.player_sprite.center_x + expected_vec[0]
+        expected_center_y = view.player_sprite.center_y - 15 + expected_vec[1]
+        
+        #on verifie si la direction est bonne avec une tolerance de 0,01 car on manipule des float
+        assert math.isclose(weapon.attribute.center_x, expected_center_x, abs_tol=0.001)
+        assert math.isclose(weapon.attribute.center_y, expected_center_y, abs_tol=0.001)
+        window.test(10)
+
+
 
     
 
